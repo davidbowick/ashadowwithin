c4660e98ae5c8f5cb0967e271240963a349072a5
David Bowick <david@designracy.com>
2025-09-29 19:17:30 -0600

    Music + features

--- sanity.config.ts
+++ sanity.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
+  structure,
   plugins: [
-    structure(),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
